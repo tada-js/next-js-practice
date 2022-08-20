6b64e8ae3f781c8bf6a54447e16e5599b782adc0
Kim Seong Hun <justfix010@gmail.com>
2022-08-21 01:26:40 +0900

    Feat : Dynamic Routes 추가

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -1,5 +1,10 @@
-export default function AboutView() {
-  return (
-    <div>ID</div>
-  )
+import { useRouter } from 'next/router'
+
+const Post = () => {
+  const router = useRouter()
+  const { id } = router.query
+
+  return <p>Post: {id}</p>
 }
+
+export default Post
