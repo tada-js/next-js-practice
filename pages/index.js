75e778eb13e1fd54dbd827d679432220f7e6e417
Kim Seong Hun <justfix010@gmail.com>
2022-08-21 02:37:18 +0900

    Feat : Loading 추가

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
-import Axios from 'axios'
-import Head from 'next/head'
-import { useEffect, useState } from 'react'
-import { Divider, Header } from 'semantic-ui-react'
-import ItemList from '../src/components/ItemList'
+import Axios from "axios";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { Divider, Header, Loader } from "semantic-ui-react";
+import ItemList from "../src/components/ItemList";
 
 export default function Home() {
-  const [list, setList] = useState([])
-  const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
-  
+  const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const API_URL =
+    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+
   function getData() {
     Axios.get(API_URL).then((res) => {
       console.log(res.data);
-      setList(res.data)
+      setList(res.data);
+      setIsLoading(false);
     });
   }
 
@@ -24,12 +28,27 @@ export default function Home() {
       <Head>
         <title>HOME | tada-js</title>
       </Head>
-      <Header as="h3" style={{paddingTop: 20}}>베스트 상품</Header>
-      <Divider /> 
-      <ItemList list={list.slice(0, 9)} />
-      <Header as="h3" style={{paddingTop: 20}}>신상품</Header>
-      <Divider /> 
-      <ItemList list={list.slice(9)} /> 
+      {isLoading && (
+        <div style={{ padding: "300px 0" }}>
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>
+      )}
+      {!isLoading && (
+        <>
+          <Header as="h3" style={{ paddingTop: 20 }}>
+            베스트 상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(0, 9)} />
+          <Header as="h3" style={{ paddingTop: 20 }}>
+            신상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(9)} />
+        </>
+      )}
     </>
-  )
+  );
 }
