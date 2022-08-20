fb952ca6df9526bf36b783aa0bb5e71c4139d948
Kim Seong Hun <justfix010@gmail.com>
2022-08-21 02:58:36 +0900

    Feat : Loding 상품 상세 페이지 추가

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -1,6 +1,7 @@
 import Axios from "axios";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { Loader } from "semantic-ui-react";
 import Item from "../../src/components/Item";
 
 const Post = () => {
@@ -8,12 +9,14 @@ const Post = () => {
   const { id } = router.query;
 
   const [item, setItem] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
 
   function getData() {
     Axios.get(API_URL).then((res) => {
       setItem(res.data);
+      setIsLoading(false);
     });
   }
 
@@ -23,7 +26,43 @@ const Post = () => {
     }
   }, [id]);
 
-  return <Item item={item} />;
+  return (
+    <>
+      {isLoading ? (
+        <div style={{ padding: "300px 0" }}>
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>
+      ) : (
+        <Item item={item} />
+      )}
+    </>
+  );
 };
 
 export default Post;
+
+/*
+Next.js 모든 페이지 사전 렌더링 (Pre-rendering)
+더 좋은 퍼포먼스
+검색엔진최적화(SEO)
+
+Per-rendering에는 2가지 종류가 있다.
+1. 정적 생성
+2. Server Side Rendering(SSR, Dynamic Rendering)
+
+두 가지 차이점은 언제 html 파일을 생성하는가?
+
+[정적 생성]
+- 프로젝트가 빌드하는 시점에 html 파일들이 생성
+- 모든 요청에 재사용
+- 퍼포먼스 이유로, next.js는 정적 생성을 권고
+- 정적 생성된 페이지들은 CDN에 캐시
+- getStaticProps / getstaticPaths
+
+[서버사이드 렌더링]
+- 매 요청마다 html 파일 생성
+- 항상 최신 상태 유지
+- getServerSideProps  
+*/
