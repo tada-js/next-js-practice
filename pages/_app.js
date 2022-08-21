46dbc5b782938a86e94b8d434d7fed5d010a4292
Kim Seong Hun <justfix010@gmail.com>
2022-08-22 01:31:25 +0900

    Add : pageProps 주석 설명 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,20 @@
-import '../styles/globals.css'
-import 'semantic-ui-css/semantic.min.css'
-import Footer from '../src/components/Footer'
-import Top from '../src/components/Top'
+import "../styles/globals.css";
+import "semantic-ui-css/semantic.min.css";
+import Footer from "../src/components/Footer";
+import Top from "../src/components/Top";
 
 function MyApp({ Component, pageProps }) {
+  // pageProps는 getStaticProps, getServerSideProps, getInitialProps를 통해 가져온 초기 속성값을 뜻하며 값이 없다면 빈 객체를 반환
   return (
-    <div style={{width: 1000, margin: "0 auto"}}>
+    <div style={{ width: 1000, margin: "0 auto" }}>
       <Top />
       <Component {...pageProps} />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
 
 // _app.js에서
 // 페이지 전환시 레이아웃을 유지할 수 있다.
