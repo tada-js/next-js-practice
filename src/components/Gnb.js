55634f8ae8fb531abc169c4ab85958d633e033f7
Kim Seong Hun <justfix010@gmail.com>
2022-08-22 00:57:36 +0900

    Style : 현재탭 상태 보여주기

--- src/components/Gnb.js
+++ src/components/Gnb.js
@@ -2,13 +2,21 @@ import { useRouter } from "next/router";
 import { Menu } from "semantic-ui-react";
 
 export default function Gnb() {
-  const activeItem = "home";
   const router = useRouter();
+  let activeItem;
+
+  if (router.pathname === "/") {
+    activeItem = "home";
+  } else if (router.pathname === "/about") {
+    activeItem = "about";
+  }
 
   function goLink(e, data) {
     if (data.name === "home") {
+      // home으로 이동
       router.push("/");
     } else if (data.name === "about") {
+      // about으로 이동
       router.push("/about");
     }
   }
