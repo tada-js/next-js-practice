6731f291c9d3130ae4a6f2db06bad5344eeff6a1
Kim Seong Hun <justfix010@gmail.com>
2022-08-22 00:39:35 +0900

    Feat : home, about 라우터

--- src/components/Gnb.js
+++ src/components/Gnb.js
@@ -1,26 +1,32 @@
+import { useRouter } from "next/router";
 import { Menu } from "semantic-ui-react";
 
 export default function Gnb() {
-  const activeItem = "home"
+  const activeItem = "home";
+  const router = useRouter();
+
+  function goLink(e, data) {
+    if (data.name === "home") {
+      router.push("/");
+    } else if (data.name === "about") {
+      router.push("/about");
+    }
+  }
+
   return (
     <>
       <Menu inverted>
         <Menu.Item
-          name='home'
-          active={activeItem === 'home'}
-          // onClick={this.handleItemClick}
+          name="home"
+          active={activeItem === "home"}
+          onClick={goLink}
         />
         <Menu.Item
-          name='messages'
-          active={activeItem === 'messages'}
-          // onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='friends'
-          active={activeItem === 'friends'}
-          // onClick={this.handleItemClick}
+          name="about"
+          active={activeItem === "about"}
+          onClick={goLink}
         />
       </Menu>
     </>
-  )
+  );
 }
